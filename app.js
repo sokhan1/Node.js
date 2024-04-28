b06456050176c76a073f72b44fc769dafa3a1576
SEOKHWAN KIM <99132328+sokhan1@users.noreply.github.com>
2024-04-28 23:13:01 +0900

    Update app.js
    app.js

--- app.js
+++ app.js
@@ -6,11 +6,25 @@ const app =express();
 app.set('port',process.env.PORT || 3000);
 
 app.use((req,res,next)=>{
-    console.log('모든 요청에 실행하고 싶어요');
+    console.log('1 요청에 실행하고 싶어요');
     next();
+},(req,res,next)=>{
+    try{
+        console.log(asdasdasd);
+    }catch(error){
+        next(error);
+    }
 })
+
+app.get('/',(req,res,next)=>{
+    res.sendFile(path.join(__dirname, 'index.html'));
+    if(true){next('route');}
+    else{next();}
+});
+
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname, 'index.html'));
+
 });
 
 app.post('/',(req,res)=>{
@@ -25,6 +39,11 @@ app.get('/about',(req,res)=>{
     res.send('hello express');
 });
 
+
+app.use((err,req,res,next)=>{
+    console.error(err);
+    res.send('error appear')
+})
 
 app.listen(app.get('port'),()=>{
     console.log('express is running');
